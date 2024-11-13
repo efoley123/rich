e5d944e126228edb97607c01586bc57502045882
Rosy122333 <80847580+Rosy122333@users.noreply.github.com>
2024-11-12 16:08:01 -0800

    test 1

--- rich/color.py
+++ rich/color.py
@@ -9,7 +9,7 @@ from ._palettes import EIGHT_BIT_PALETTE, STANDARD_PALETTE, WINDOWS_PALETTE
 from .color_triplet import ColorTriplet
 from .repr import Result, rich_repr
 from .terminal_theme import DEFAULT_TERMINAL_THEME
-#test 
+#test this
 if TYPE_CHECKING:  # pragma: no cover
     from .terminal_theme import TerminalTheme
     from .text import Text
